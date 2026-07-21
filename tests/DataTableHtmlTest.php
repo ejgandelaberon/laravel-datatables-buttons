f31be1079dfa42cd2f4ca3a6884d9f6ae2105b76
Emmanuel Joseph Beron <ejberon@objectbright.com>
2026-07-21 12:38:09 +0800

    test(html): cover zero-argument container resolution

--- tests/DataTableHtmlTest.php
+++ tests/DataTableHtmlTest.php
@@ -8,6 +8,14 @@ use Yajra\DataTables\Html\DataTableHtml;
 class DataTableHtmlTest extends TestCase
 {
     #[Test]
+    public function it_resolves_constructor_dependencies_through_the_container_without_explicit_arguments(): void
+    {
+        $builder = ContainerResolvedDataTableHtml::make();
+
+        $this->assertSame('container-users', $builder->getTableId());
+    }
+
+    #[Test]
     public function it_resolves_the_static_class_with_positional_arguments_through_the_container(): void
     {
         $repository = app(DataTableHtmlUserRepository::class);
@@ -21,7 +29,7 @@ class DataTableHtmlTest extends TestCase
 
 class ContainerResolvedDataTableHtml extends DataTableHtml
 {
-    public function __construct(DataTableHtmlUserRepository $repository, string $tableId)
+    public function __construct(DataTableHtmlUserRepository $repository, string $tableId = 'users')
     {
         $this->tableId = $repository->tablePrefix.$tableId;
     }
